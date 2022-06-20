3624b96a7fa25f520c15c224c13e938401d43c05
purpleeeee <h.purpleeeee@gmail.com>
2022-06-20 23:50:09 +0900

    add Document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app"
 import { GlobalStyle } from "@/shared/lib/globalStyle"
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyle />
