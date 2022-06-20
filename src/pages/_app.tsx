fbd74b94d6a16f3fd2eddb39c39e7978bad1664f
purpleeeee <h.purpleeeee@gmail.com>
2022-06-20 23:55:25 +0900

    add Meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app"
-import { GlobalStyle } from "@/shared/lib/globalStyle"
+import { GlobalStyle } from "@/shared/lib/style/globalStyle"
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
---
+++ src/shared/lib/meta/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head"
+
+export const Meta = () => {
+  return (
+    <Head>
+      <title>🟣</title>
+      <link rel="icon" href="/favicon.ico" />
+      {/* wip */}
+    </Head>
+  )
+}
