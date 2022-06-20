0c80517c606cf3bd36d9bfe01f4a81e46660c7a8
purpleeeee <h.purpleeeee@gmail.com>
2022-06-21 01:20:22 +0900

    fix: active link

--- src/shared/components/AppLink/index.tsx
+++ src/shared/components/AppLink/index.tsx
@@ -1,11 +1,22 @@
 import Link, { LinkProps } from "next/link"
 import { useRouter } from "next/router"
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, useEffect, useState } from "react"
 import { A } from "./styled"
 
 export const AppLink = (props: PropsWithChildren<LinkProps>) => {
   const router = useRouter()
-  const isActive = router.asPath === props.href
+
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    if (!router.isReady) return
+    if (router.asPath === props.href) {
+      setIsActive(true)
+    } else {
+      setIsActive(false)
+    }
+  }, [router.isReady, router.asPath, props.href])
+
   return (
     <Link {...props} passHref>
       <A isActive={isActive}>{props.children}</A>
