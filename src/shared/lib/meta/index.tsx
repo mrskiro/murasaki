78a8cb55082a4e206a52db4abc3b661971ae07d7
purpleeeee <h.purpleeeee@gmail.com>
2022-06-21 01:51:46 +0900

    change favicon

--- src/shared/lib/meta/index.tsx
+++ src/shared/lib/meta/index.tsx
@@ -8,7 +8,10 @@ export const Meta = (props: Props) => {
   return (
     <Head>
       <title>{`${props.title} | 🟣`}</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🟣</text></svg>"
+      />
       {/* wip */}
     </Head>
   )
