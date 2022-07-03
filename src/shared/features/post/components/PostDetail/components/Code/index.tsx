819d5619626ead6044e4fc3b1195a7faa9b17e85
purpleeeee <h.purpleeeee@gmail.com>
2022-07-04 02:13:59 +0900

    add syntax highlight

--- src/shared/features/post/components/PostDetail/components/Code/index.tsx
+++ src/shared/features/post/components/PostDetail/components/Code/index.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react"
+import styled from "styled-components"
+import Prism from "prismjs"
+
 import { RichText } from "@/shared/features/post/types"
+import "prismjs/plugins/line-numbers/prism-line-numbers.js"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+
+import "prismjs/themes/prism-okaidia.css"
+import "prismjs/components/prism-clike.min.js"
+import "prismjs/components/prism-javascript.min.js"
+import "prismjs/components/prism-js-extras.min.js"
+import "prismjs/components/prism-typescript.min.js"
+import "prismjs/components/prism-jsx.min.js"
+import "prismjs/components/prism-tsx.min.js"
+import "prismjs/components/prism-json.min.js"
+import "prismjs/components/prism-css.min.js"
+import "prismjs/components/prism-css-extras.min.js"
 
 type Props = {
   text: RichText
@@ -6,9 +23,20 @@ type Props = {
 }
 
 export const Code = (props: Props) => {
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
+
   return (
-    <pre>
-      <code>{props.text.plainText}</code>
-    </pre>
+    <Pre className="line-numbers">
+      <code className={`language-${props.language}`}>
+        {props.text.plainText}
+      </code>
+    </Pre>
   )
 }
+
+const Pre = styled.pre`
+  // Prismが優先度高いセレクタで当ててくるので絶対に勝つ
+  font-size: 12px !important;
+`
