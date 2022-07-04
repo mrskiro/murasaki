6b921d1eddc0c601241cca33fd03f31afcb65b37
purpleeeee <h.purpleeeee@gmail.com>
2022-07-04 23:10:34 +0900

    add about meta

--- src/pages/about/index.page.tsx
+++ src/pages/about/index.page.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next"
 import { load } from "@/shared/lib/config"
+import { Meta } from "@/shared/lib/meta"
 import { findPostDetailById } from "@/shared/features/post/api"
 import { PostDetail } from "@/shared/features/post/components/PostDetail"
 import * as PostTypes from "@/shared/features/post/types"
@@ -19,7 +20,12 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 }
 
 const Page = (props: Props) => {
-  return <PostDetail postDetail={props.aboutPageDetail} />
+  return (
+    <>
+      <Meta title="About" />
+      <PostDetail postDetail={props.aboutPageDetail} />
+    </>
+  )
 }
 
 export default Page
--- src/shared/layouts/Main/Navigation/index.tsx
+++ src/shared/layouts/Main/Navigation/index.tsx
@@ -2,22 +2,30 @@ import { useRouter } from "next/router"
 import { AppLink } from "@/shared/components/AppLink"
 import * as S from "./styled"
 
+const lists = [
+  {
+    name: "Posts",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+]
+
 export const Navigation = () => {
   const router = useRouter()
 
   return (
     <S.Wrap>
       <S.Ul>
-        <li>
-          <AppLink href="/" isActive={router.pathname === "/root"}>
-            Posts
-          </AppLink>
-        </li>
-        <li>
-          <AppLink href="/about" isActive={router.pathname === "/about"}>
-            About
-          </AppLink>
-        </li>
+        {lists.map((v) => (
+          <li key={v.name}>
+            <AppLink href={v.href} isActive={router.asPath === v.href}>
+              {v.name}
+            </AppLink>
+          </li>
+        ))}
       </S.Ul>
     </S.Wrap>
   )
