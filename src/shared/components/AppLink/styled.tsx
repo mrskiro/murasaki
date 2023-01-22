5db74f44f9aa319cb11789b3e01544a0f99d5d69
purpleeeee <h.purpleeeee@gmail.com>
2023-01-22 19:52:23 +0900

    fixup

--- src/shared/components/AppLink/styled.tsx
+++ src/shared/components/AppLink/styled.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components"
 
 export const A = styled.a`
-  cursor: pointer;
-  text-decoration: underline;
+    cursor: pointer;
+    text-decoration: underline;
+    word-break: break-all;
 `
 
 export const Nav = styled.a<{ isActive?: boolean }>`
-  cursor: pointer;
-  text-decoration: ${(props) => (props.isActive ? "underline" : "inherit")};
-  text-underline-offset: 4px;
+    cursor: pointer;
+    text-decoration: ${(props) => (props.isActive ? "underline" : "inherit")};
+    text-underline-offset: 4px;
 `
