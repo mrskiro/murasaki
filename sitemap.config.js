985015e9740f9ce620448509e5b8cd4b97bf6d8e
purpleeeee <h.purpleeeee@gmail.com>
2023-02-06 00:46:29 +0900

    chore

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,11 +1,10 @@
-/** @type {import('next-sitemap').IConfig} */
-
 const URL = process.env.SITE_URL
 
 if (!URL) {
   throw new Error("not exist site url")
 }
 
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: URL,
   generateRobotsTxt: true,
