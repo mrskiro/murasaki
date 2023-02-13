b47a7ad588a303bc672fbe1948662653a25df689
purpleeeee <h.purpleeeee@gmail.com>
2023-02-13 21:03:29 +0900

    reg e2e

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -1,15 +1,7 @@
-import { test, expect } from "@playwright/test"
+import { test, expect, Page } from "@playwright/test"
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/")
-})
-
-test("has title", async ({ page }) => {
-  await expect(page).toHaveTitle(/🟣🟣🟪🟪/)
-  await page.goto(`/about`)
-  await expect(page).toHaveTitle(/About/)
-  await page.goto(`/posts/demo`)
-  await expect(page).toHaveTitle(/demo/)
 })
 
 test("screen shots", async ({ page }) => {
@@ -21,18 +13,44 @@ test("screen shots", async ({ page }) => {
     fullPage: true,
   })
   await page.goto(`/posts/demo`)
+
+  await scrollToBottom(page)
+
+  await scrollToTop(page)
+
   await expect(page).toHaveScreenshot("post.png", {
     fullPage: true,
   })
 })
 
 test("navigation", async ({ page }) => {
+  await expect(page).toHaveTitle(/🟣🟣🟪🟪/)
+
   await page.getByRole("link", { name: "About" }).click()
   await expect(page).toHaveURL(/about/)
+  await expect(page).toHaveTitle(/About/)
 
   await page.getByRole("link", { name: "Posts" }).click()
   await expect(page).not.toHaveURL(/about/)
 
   await page.getByRole("link", { name: "demo" }).click()
-  await expect(page).not.toHaveURL(/.demo/)
+  await expect(page).toHaveURL(/demo/)
+  await expect(page).toHaveTitle(/demo/)
 })
+
+const scrollToBottom = async (page: Page) => {
+  await page.evaluate(() => {
+    const browserHeight = window.innerHeight
+    const pageHeight = document.body.scrollHeight
+
+    for (let i = 0; i < pageHeight; i += browserHeight) {
+      window.scrollTo(0, i)
+    }
+  })
+}
+
+const scrollToTop = async (page: Page) => {
+  await page.evaluate(() => {
+    window.scrollTo(0, 0)
+  })
+}
--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    baseURL: "http://localhost:3333",
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -70,8 +70,8 @@ export default defineConfig({
   // outputDir: "./e2e/snapshots",
 
   webServer: {
-    command: "yarn dev:e2e",
-    url: "http://localhost:3333",
+    command: "yarn start",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
 })
