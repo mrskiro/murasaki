d3ccf4046a3191b4c183a49f51ada5de5f791ea9
mrskiro <mrskhrk@gmail.com>
2024-01-01 01:28:58 +0900

    fix: meta

--- src/lib/meta/index.tsx
+++ src/lib/meta/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { useRouter } from "next/router"
 
 type Props = {
   title: string
@@ -10,16 +11,17 @@ export const Meta = (props: Props) => {
   const og = `https://og-image-eta-sable.vercel.app/${encodeURIComponent(
     props.title
   )}`
-  // TODO: envに移動
-  const fullPath = `https://${
-    process.env.NEXT_PUBLIC_VERCEL_URL || "mrskiro.dev"
-  }`
+
+  const router = useRouter()
+  const url = `https://mrskiro.dev${router.asPath}`
+
   return (
     <Head>
       <link
         rel="icon"
         href="data:image/svg+xml,&lt;svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22&gt;&lt;text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22&gt;🟣&lt;/text&gt;&lt;/svg&gt;"
       />
+      <link rel="canonical" href={url} />
       {/* TODO: envに移動 */}
       <title>{`${props.title} | mrskiro.dev`}</title>
 
@@ -27,13 +29,12 @@ export const Meta = (props: Props) => {
         <meta name="description" content={props.description} />
       )}
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content="@purp1eeeee" />
+      <meta name="twitter:card" content="summary" />
 
       <meta property="og:type" content={props.ogType} />
       <meta property="og:title" content={props.title} />
       <meta property="og:image" content={og} />
-      <meta property="og:url" content={fullPath} />
+      <meta property="og:url" content={url} />
       {/* TODO: envに移動 */}
       <meta property="og:site_name" content="mrskiro.dev" />
       <meta property="og:image:width" content="1200" />
