967f150d822e4833d4c3cbe9a37db7a02cc956a0
mrskiro <mrskhrk@gmail.com>
2024-01-01 01:46:52 +0900

    fix: meta 2

--- src/lib/meta/index.tsx
+++ src/lib/meta/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const Meta = (props: Props) => {
   const og = `https://og-image-eta-sable.vercel.app/${encodeURIComponent(
     props.title
-  )}`
+  )}.png`
 
   const router = useRouter()
   const url = `https://mrskiro.dev${router.asPath}`
@@ -30,13 +30,13 @@ export const Meta = (props: Props) => {
       )}
 
       <meta name="twitter:card" content="summary" />
-
-      <meta property="og:type" content={props.ogType} />
-      <meta property="og:title" content={props.title} />
-      <meta property="og:image" content={og} />
+      <meta name="twitter:title" content={props.title} />
       <meta property="og:url" content={url} />
-      {/* TODO: envに移動 */}
+      <meta property="og:title" content={props.title} />
+      <meta property="og:type" content={props.ogType} />
       <meta property="og:site_name" content="mrskiro.dev" />
+
+      <meta property="og:image" content={og} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
     </Head>
