2fe7ecca3ec96dc654855023ea81662130095ec1
mrskiro <mrskhrk@gmail.com>
2024-03-10 13:09:14 +0900

    fix: bmc link

--- src/lib/bmc/bmc-button.tsx
+++ src/lib/bmc/bmc-button.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import { AppLink } from "@/components/app-link"
 
 export const BmcButton = () => (
-  <AppLink isExternal href="https://www.buymeacoffee.com/mur4saki">
+  <AppLink isExternal href="https://www.buymeacoffee.com/mrskiro">
     <Image
       src="/assets/bmc.png"
       alt="by me a coffee"
