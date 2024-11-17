109331d232c925f2bfb56b52d7bf2b6777f7b71e
mrskiro <mrskiro.h@gmail.com>
2024-11-17 17:30:19 +0900

    fix: styling

--- src/features/post/components/posts/components/post-item/components/tag/index.tsx
+++ src/features/post/components/posts/components/post-item/components/tag/index.tsx
@@ -8,26 +8,30 @@ export const ExternalTag = (props: Props) => {
   const isZenn = props.link.includes("zenn");
   if (isZenn) {
     return (
-      <Image
-        src="/assets/zenn-logo.png"
-        alt="zenn"
-        width={52}
-        height={20}
-        objectFit="contain"
-      />
+      <div>
+        <Image
+          src="/assets/zenn-logo.png"
+          alt="zenn"
+          width={52}
+          height={20}
+          objectFit="contain"
+        />
+      </div>
     );
   }
 
   const isQiita = props.link.includes("qiita");
   if (isQiita) {
     return (
-      <Image
-        src="/assets/qiita-logo.png"
-        alt="qiita"
-        width={52}
-        height={20}
-        objectFit="contain"
-      />
+      <div>
+        <Image
+          src="/assets/qiita-logo.png"
+          alt="qiita"
+          width={52}
+          height={20}
+          objectFit="contain"
+        />
+      </div>
     );
   }
 
